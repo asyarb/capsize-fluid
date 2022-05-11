ae367667145332fc7daf85e4df5f4190ec6202fe
Anthony Yarbrough <asyarb@hawaii.edu>
2022-05-11 13:11:29 -1000

    feat: more init

--- src/index.ts
+++ src/index.ts
@@ -1,33 +1,3 @@
-import creator from 'tailwindcss/plugin'
-
-import { type Options } from './validators'
-import { createContext } from './context'
-import { ensureSameFontKeys } from './ensureSameFontKeys'
-import { createFontFamilyUtils } from './createFontFamilyUtils'
-import { createFontSizeUtils } from './createFontSizeUtils'
-import { createLineHeightUtils } from './createLineHeightUtils'
-import { createCapsizeUtil } from './createCapsizeUtil'
-import { createRootVariables } from './createRootVariables'
-
-const DEFAULT_CONFIG = {
-  corePlugins: {
-    fontSize: false,
-    lineHeight: false,
-    fontFamily: false,
-  },
+export function foo() {
+  console.log('hello world')
 }
-
-const tailwindCapsize = creator.withOptions<Partial<Options>>(
-  (options) => (tw) => {
-    const ctx = createContext(options, tw)
-    ensureSameFontKeys(ctx)
-    createRootVariables(ctx)
-    createFontSizeUtils(ctx)
-    createLineHeightUtils(ctx)
-    createFontFamilyUtils(ctx)
-    createCapsizeUtil(ctx)
-  },
-  (_options) => DEFAULT_CONFIG
-)
-
-export default tailwindCapsize
