3ff7770cd0c69e775689504264cae2b7ecb81ca9
Anthony Yarbrough <asyarb@hawaii.edu>
2022-05-11 14:42:26 -1000

    feat: working implementation

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,108 @@
-export function foo() {
-  console.log('hello world')
+import { precomputeValues, type FontMetrics } from '@capsizecss/core'
+
+function v(min: FluidFontSize, max: FluidFontSize) {
+  const numerator = 100 * (max.fontSize - min.fontSize)
+  const denominator = max.screenSize - min.screenSize
+
+  return numerator / denominator
+}
+
+function r(min: FluidFontSize, max: FluidFontSize) {
+  const numerator =
+    min.screenSize * max.fontSize - max.screenSize * min.fontSize
+  const denominator = min.screenSize - max.screenSize
+
+  return numerator / denominator
+}
+
+export interface FluidFontSize {
+  fontSize: number
+  screenSize: number
+}
+
+export interface FluidCapsizeOptions {
+  min: FluidFontSize
+  max: FluidFontSize
+  lineHeight: number
+  rootFontSize: number
+  fontMetrics: FontMetrics
+}
+
+export function precomputeFluidValues({
+  max,
+  min,
+  lineHeight,
+  rootFontSize,
+  fontMetrics,
+}: FluidCapsizeOptions) {
+  const { baselineTrim, capHeightTrim } = precomputeValues({
+    fontMetrics,
+    fontSize: min.fontSize,
+    leading: lineHeight * min.fontSize,
+  })
+
+  const vw = v(min, max)
+  const rem = r(min, max)
+
+  return {
+    minFontSize: min.fontSize / rootFontSize + 'rem',
+    maxFontSize: max.fontSize / rootFontSize + 'rem',
+    baselineTrim,
+    capHeightTrim,
+    vw: Number.parseFloat(vw.toFixed(4)) + 'vw',
+    rem: rem / rootFontSize + 'rem',
+  }
+}
+
+export function createFluidStyleObject(arg: FluidCapsizeOptions) {
+  const values = precomputeFluidValues(arg)
+
+  return {
+    fontSize: `clamp(${values.minFontSize}, ${values.vw} + ${values.rem}, ${values.maxFontSize})`,
+    lineHeight: arg.lineHeight.toString(),
+
+    '::before': {
+      content: '""',
+      display: 'table',
+      marginBottom: values.capHeightTrim,
+    },
+
+    '::after': {
+      content: '""',
+      display: 'table',
+      marginTop: values.baselineTrim,
+    },
+  }
+}
+
+export function createFluidStyleString(
+  ruleName: string,
+  arg: FluidCapsizeOptions
+) {
+  const {
+    '::after': afterPseudo,
+    '::before': beforePseudo,
+    ...rootStyles
+  } = createFluidStyleObject(arg)
+
+  const objToCSSRules = <Property extends string>(
+    stylesObj: Record<Property, string>,
+    psuedoName?: string
+  ) => `
+.${ruleName}${psuedoName ? `::${psuedoName}` : ''} {
+${Object.keys(stylesObj)
+  .map(
+    (property) =>
+      `  ${property.replace(/[A-Z]/g, '-$&').toLowerCase()}: ${stylesObj[
+        property as keyof typeof stylesObj
+      ].replace(/'/g, '"')}`
+  )
+  .join(';\n')};
+}`
+
+  return [
+    objToCSSRules(rootStyles),
+    objToCSSRules(beforePseudo, 'before'),
+    objToCSSRules(afterPseudo, 'after'),
+  ].join('\n')
 }
--- tests/index.test.ts
+++ tests/index.test.ts
@@ -1,3 +1,76 @@
-import { test } from 'vitest'
+import { test, expect } from 'vitest'
+import interFontMetrics from '@capsizecss/metrics/inter'
 
-test('works', () => {})
+import {
+  precomputeFluidValues,
+  createFluidStyleObject,
+  createFluidStyleString,
+} from '../src'
+
+const args = {
+  max: { fontSize: 36, screenSize: 1440 },
+  min: { fontSize: 24, screenSize: 360 },
+  fontMetrics: interFontMetrics,
+  lineHeight: 1.25,
+  rootFontSize: 16,
+}
+
+test('precomputeFluidValues', () => {
+  const result = precomputeFluidValues(args)
+
+  expect(result).toMatchInlineSnapshot(`
+    {
+      "baselineTrim": "-0.2614em",
+      "capHeightTrim": "-0.2614em",
+      "maxFontSize": "2.25rem",
+      "minFontSize": "1.5rem",
+      "rem": "1.25rem",
+      "vw": "1.1111vw",
+    }
+  `)
+})
+
+test('createFluidStyleObject', () => {
+  const result = createFluidStyleObject(args)
+
+  expect(result).toMatchInlineSnapshot(`
+    {
+      "::after": {
+        "content": "\\"\\"",
+        "display": "table",
+        "marginTop": "-0.2614em",
+      },
+      "::before": {
+        "content": "\\"\\"",
+        "display": "table",
+        "marginBottom": "-0.2614em",
+      },
+      "fontSize": "clamp(1.5rem, 1.1111vw + 1.25rem, 2.25rem)",
+      "lineHeight": "1.25",
+    }
+  `)
+})
+
+test('createFluidStyleString', () => {
+  const result = createFluidStyleString('fluid-capsize', args)
+
+  expect(result).toMatchInlineSnapshot(`
+    "
+    .fluid-capsize {
+      font-size: clamp(1.5rem, 1.1111vw + 1.25rem, 2.25rem);
+      line-height: 1.25;
+    }
+    
+    .fluid-capsize::before {
+      content: \\"\\";
+      display: table;
+      margin-bottom: -0.2614em;
+    }
+    
+    .fluid-capsize::after {
+      content: \\"\\";
+      display: table;
+      margin-top: -0.2614em;
+    }"
+  `)
+})
